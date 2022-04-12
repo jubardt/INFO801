5f3289787c40893fdc07e7664c22da83f38869e5
mrBELLAZIA <73542412+mrBELLAZIA@users.noreply.github.com>
2022-04-12 14:01:03 +0200

    bah bah abha ab

--- MaitreOeuvre_Client/Client/app/src/components/listes.vue
+++ MaitreOeuvre_Client/Client/app/src/components/listes.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>Requête</h1>
+  <div class="all">
+  <h1 class="title">Requête</h1>
 
   <div v-for="element in listeRequete" :key="element.id">
-    <h2>{{element}}</h2>
-  </div>
+    <div class="box container-req">
+      <h2 class="title">{{element.fabriquant}}</h2>
+      <h3>Cout : {{element.cout}}</h3>
+      <h3>Délai : {{element.delai}}</h3>
+      <h3>Quantité : {{element.quantite}}</h3>
+      <h2 class="subtitle">Contre-proposition</h2>
+      <h4>Cout : {{element.proposition.cout}}</h4>
+      <h4>Délai : {{element.proposition.delai}}</h4>
+      <h4>Quantité : {{element.proposition.quantite}}</h4>
+      <h4>Sujet : {{element.proposition.sujet}}</h4>
 
-  <button class="button" @click="test()">test</button>
+      <div v-for="el in element.proposition.caracteristiques" :key="el.id">
+        <h4>{{el}}</h4>
+      </div>
+
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -16,14 +31,16 @@ export default {
   name: "listes",
   data() {
     return {
-      listeRequete: ["caca", "coucou", "salut"]
+      listeRequete: [],
+      isLoaded: false,
     }
   },
   mounted() {
     request('http://localhost:3000/contreprop', { json: true }, (err, res) => {
       if (err) { return console.log(err); }
       console.log(res.body);
-      listeRequete = res.body.liste;
+      this.listeRequete = res.body.liste;
+      this.isLoaded = true;
       console.log(listeRequete.length);
     });
   },
@@ -39,5 +56,22 @@ export default {
 </script>
 
 <style scoped>
+.box{
+  color: white;
+  background-color: #2196F3;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+}
+
+.all{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #95794B;
+  height: 100vh;
+  width: 100vw;
+}
+
 
 </style>
